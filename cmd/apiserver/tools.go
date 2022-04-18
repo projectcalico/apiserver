354e9982608a97bd93fc67e06f42608849a341ed
Casey Davenport <davenport.cas@gmail.com>
2022-04-18 10:15:05 -0700

    fix static checks

--- cmd/apiserver/tools.go
+++ cmd/apiserver/tools.go
@@ -1,7 +1,4 @@
-//go:build tools
-// +build tools
-
-// Copyright (c) 2021 Tigera, Inc. All rights reserved.
+// Copyright (c) 2021,2022 Tigera, Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,6 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+//go:build tools
+// +build tools
 
 package tools
 
